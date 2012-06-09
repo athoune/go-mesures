5a9549b580d9e649d9a8b5ea7829e4f65d2effdb
Mathieu Lecarme <mathieu@garambrogne.net>
2012-06-09 22:52:41 +0200

    Response as a struct.

--- tcpsocket.go
+++ tcpsocket.go
@@ -5,17 +5,22 @@ import (
 	"strconv"
 )
 
+type msg struct {
+    key string
+    value int64
+}
+
 var reParse *regexp.Regexp
 
 func init() {
 	reParse = regexp.MustCompile("([a-zA-Z._0-9]+)\\s+(\\d+)")
 }
 
-func parse_cmd(s string) (string, int64, bool) {
+func parse_cmd(s string) (msg, bool) {
 	results := reParse.FindStringSubmatch(s)
 	if results == nil {
-		return "", 0, false
+		return msg{"", 0}, false
 	}
 	value, _ := strconv.ParseInt(results[2], 10, 64)
-	return results[1], value, true
+	return msg{results[1], value}, true
 }
--- tcpsocket_test.go
+++ tcpsocket_test.go
@@ -5,12 +5,12 @@ import (
 )
 
 func TestLanguage(t *testing.T) {
-	key, value, ok := parse_cmd("popo 42")
-	if !ok || key != "popo" || value != 42 {
+	kv, ok := parse_cmd("popo 42")
+	if !ok || kv.key != "popo" || kv.value != 42 {
 		t.Log("bad parsing")
 		t.Fail()
 	}
-	key, value, ok = parse_cmd("popo42")
+	kv, ok = parse_cmd("popo42")
 	if ok {
 		t.Log("parse bad text")
 		t.Fail()
